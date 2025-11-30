de16d410ae56922256d951e88e480177abb86059
Loamy88 <euan.m.obrien@gmail.com>
2025-11-30 08:17:09 -0800

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import * as CANNON from 'cannon-es';
-import Stats from 'three/examples/jsm/libs/stats.module.js';
+import Stats from 'three/examples/jsm/libs/stats.module';
 import Board from './board.js';
 import Player from './player.js';
 import CameraController from './cameraController.js';
@@ -717,3 +717,4 @@ window.addEventListener('load', () => {
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = ShuffleboardGame;
 }
+
